04e71745af404251964bd91ebe1c5751436d3918
hetekce <hetekce49@gmail.com>
2020-10-22 23:42:25 +0200

    datatypes has been studied

--- intro.js
+++ intro.js
@@ -1,5 +1,43 @@
+//code to track:
+//ilk olarak console açıyoruz
+//tsc intro.ts diyerek ts dosyasının ismini girerek kodu js'ye dönüştürüyoruz.
+//Sonra yeni bir console açıyoruz, nodemon intro.js kodu ile otomatik takibe başlıyoruz.
+//artık ts dosyası üzerinde değişiklik yapıp ilk console'da tekrardan intro.ts dersek
+//otomatik olarak takip gerçekleşir ve bunu 2. console üzerinden görebiliriz.
 function greeter(name) {
     return "Welcome " + name;
 }
 var mesaj = greeter('EmRe');
 console.log(mesaj);
+var mesaj2 = greeter("Murtaza");
+console.log(mesaj2);
+//Datatypes
+var num;
+num = 10;
+num = 10.4;
+var city = "Adana"; //bu şekilde de tanımlanabilir.
+city = "Ankara";
+city = "İstanbul";
+var isTrue;
+isTrue = false;
+isTrue = true;
+var numbers = [1, 2, 3]; //arrays bu şekilde tanımlanabilir.
+var numbers2 = [1, 2, 3]; //generic array tanımı
+var dizi = [2, "Ankara"]; //bu şekilde tuple oluşturulabilir.
+dizi[0]; //2
+dizi[1]; //"Ankara"
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 1] = "Red";
+    Color[Color["Black"] = 2] = "Black";
+    Color[Color["Blue"] = 3] = "Blue";
+})(Color || (Color = {}));
+var color = Color.Red;
+var value = "Ankara"; //türünü bilmiyorsak gelen ifadenin any kullanırız.
+value = 2;
+value = {};
+var value2 = undefined; //herhangi birşey return etmek istemiyorsak.
+function giveHello() {
+    console.log("Hello");
+    //return "Hello" // bu mesela çalışmaz çünkü fonksiyon void bir fonksiyon.
+}
--- intro.ts
+++ intro.ts
@@ -1,3 +1,10 @@
+//code to track:
+//ilk olarak console açıyoruz
+//tsc intro.ts diyerek ts dosyasının ismini girerek kodu js'ye dönüştürüyoruz.
+//Sonra yeni bir console açıyoruz, nodemon intro.js kodu ile otomatik takibe başlıyoruz.
+//artık ts dosyası üzerinde değişiklik yapıp ilk console'da tekrardan intro.ts dersek
+//otomatik olarak takip gerçekleşir ve bunu 2. console üzerinden görebiliriz.
+
 function greeter(name: string){
     return "Welcome " +name
 }
@@ -5,3 +12,53 @@ function greeter(name: string){
 let mesaj = greeter('EmRe')
 
 console.log(mesaj)
+
+let mesaj2 = greeter("Murtaza")
+console.log(mesaj2);
+
+
+//Datatypes
+
+let num:number
+num = 10
+num = 10.4
+
+let city:string = "Adana" //bu şekilde de tanımlanabilir.
+city = "Ankara"
+city = "İstanbul"
+
+let isTrue :boolean
+isTrue =false
+isTrue = true
+
+let numbers:number[]=[1,2,3] //arrays bu şekilde tanımlanabilir.
+let numbers2:Array<number> =[1,2,3] //generic array tanımı
+
+let dizi:[number,string] =[2,"Ankara"] //bu şekilde tuple oluşturulabilir.
+dizi[0] //2
+dizi[1] //"Ankara"
+
+enum Color{Red=1,Black=2,Blue=3}
+let color:Color = Color.Red
+
+let value:any ="Ankara" //türünü bilmiyorsak gelen ifadenin any kullanırız.
+value = 2
+value ={}
+
+let value2:void = undefined //herhangi birşey return etmek istemiyorsak.
+
+function giveHello():void{
+    console.log("Hello")
+    //return "Hello" // bu mesela çalışmaz çünkü fonksiyon void bir fonksiyon.
+}
+
+
+
+
+
+
+
+
+
+
+
