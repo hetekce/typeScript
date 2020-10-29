dfa414c7159d8d66c6c8972b83336a0fad74878e
hetekce <hetekce49@gmail.com>
2020-10-29 08:28:39 +0100

    loops

---
+++ loops.ts
@@ -0,0 +1,8 @@
+let cities = ["Ankara", "İstanbul","İzmir"]
+for(let i in cities){ //bununla index numaraları yazdırılır.
+    console.log(i)
+}
+
+for(let i of cities){ //bununla elemanlar yazdırılır.
+    console.log(i)
+}
